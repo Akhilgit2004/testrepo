75673a15f038b25828cdd5c8616fc55794904b89
Akhilgit2004 <malladi.akhil666@gmail.com>
2026-05-06 21:08:48 +0530

    Test3

--- app.cpp
+++ app.cpp
@@ -6,8 +6,6 @@ int main() {
     for (int i = 0; i < 3; i++) {
         int result = i * multiplier;
     }
-    
-    // Bug: 'result' was declared inside the loop and doesn't exist here
     std::cout << "Final result: " << result << std::endl;
     
     return 0;
