f33f791c3a9d58d0f8a2a212b107003fae0a639e
Akhilgit2004 <malladi.akhil666@gmail.com>
2026-05-08 19:18:19 +0530

    added test case

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Booting up system...");
+        
+        
+        Server.start(); 
+    }
+}
---
+++ Server.java
@@ -0,0 +1,6 @@
+public class Server {
+    public static void initialize() {
+        System.out.println("Server is warming up...");
+        System.out.println("Server initialized successfully on Port 8080!");
+    }
+}
